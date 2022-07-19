26f0a37754f944adf78724df15f98114b0322596
abhikesare9 <abhikesare9@gmail.com>
2022-07-19 11:01:28 +0530

    user microservice code

---
+++ user/app.py
@@ -0,0 +1,41 @@
+
+from flask import Flask
+from routes import user_blueprint
+from flask_migrate import Migrate
+import models
+from flask_login import LoginManager
+from flask import g
+from flask.sessions import SecureCookieSessionInterface
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'CJEnExvPBocmKR5O0cMJsw'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database/user.db'
+models.init_app(app)
+app.register_blueprint(user_blueprint)
+login_manager = LoginManager(app)
+migrate = Migrate(app,models.db)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return models.User.query.filter_by(id=user_id).first()
+
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic','',1)
+        user = models.User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+    return None
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+
+    def save_session(self, *args,**kwargs):
+        if g.get('login_via_loader'):
+            return
+        return super(CustomSessionInterface,self).save_session(*args,**kwargs)
+if __name__ == "main":
+    app.run(port=1111,debug=True)
